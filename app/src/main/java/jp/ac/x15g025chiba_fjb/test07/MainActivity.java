3ec0320737ae8aaf63cd8d02e777b78f80444279
WIN_NT\x15g025 <x15g025@chiba-fjb.ac.jp>
2017-05-01 12:06:10 +0900

    ID

---
+++ app/src/main/java/jp/ac/x15g025chiba_fjb/test07/MainActivity.java
@@ -0,0 +1,43 @@
+package jp.ac.x15g025chiba_fjb.test07;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //インスタンスの取得
+        Button b1 = (Button)findViewById(R.id.b1);
+        Button b2 = (Button)findViewById(R.id.b2);
+        Button b3 = (Button)findViewById(R.id.b3);
+        Button b4 = (Button)findViewById(R.id.b4);
+        //イベントの設定
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+
+        //クラスのプロパティにする
+        mTextView = (TextView)findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.b1)
+            mTextView.setText("ボタン１がおされました");
+        else if(v.getId() == R.id.b2)
+            mTextView.setText("ボタン２がおされました");
+        else if(v.getId() == R.id.b3)
+            mTextView.setText("ボタン３がおされました");
+        else if(v.getId() == R.id.b4)
+            mTextView.setText("ボタン４がおされました");
+    }
+}
